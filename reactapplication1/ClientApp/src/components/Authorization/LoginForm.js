edc4aafbe20ca74e559e410080ea41e3e460389e
Harald Korneliussen <harald.korneliussen@cordel.no>
2018-12-10 15:11:41 +0100

    typescript-dependency lagt til (igjen) og første fil oversatt til typescript. Kode i LoginForm som Typescriptcompileren ikke godtar (selv i javascript) fikset.

--- reactapplication1/ClientApp/src/components/Authorization/LoginForm.js
+++ reactapplication1/ClientApp/src/components/Authorization/LoginForm.js
@@ -4,27 +4,23 @@ export default class Login extends Component {
 
     render() {
         const { errorMessage } = this.props
-
         return (
             <div>
-                <input type='text' ref='username' className="form-control" style= placeholder='Username' />
-                <input type='password' ref='password' className="form-control" style= placeholder='Password' />
+                <input type='text' ref='username' className="form-control" placeholder='Username' />
+                <input type='password' ref='password' className="form-control" placeholder='Password' />
                 <button onClick={(event) => this.handleClick(event)} className="btn btn-primary">
                     Login
-        </button>
+                </button>
+            </div>
+        )
+    }
 
-               
-        
-      </div>
-    )
-  }
-
-handleClick(event) {
-    const username = this.refs.username
-    const password = this.refs.password
-    const creds = { username: username.value.trim(), password: password.value.trim() }
-    this.props.onLoginClick(creds)
-}
+    handleClick(event) {
+        const username = this.refs.username
+        const password = this.refs.password
+        const creds = { username: username.value.trim(), password: password.value.trim() }
+        this.props.onLoginClick(creds)
+    }
 }
 
 Login.propTypes = {
